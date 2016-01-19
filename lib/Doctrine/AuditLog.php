b5fbfcd4dba535b879c973ce41c3a144ed775c9c
Dirk Nederveen <dirk@recras.nl>
2016-01-19 15:08:24 +0100

    Find SoftDelete column name dynamically

--- lib/Doctrine/AuditLog.php
+++ lib/Doctrine/AuditLog.php
@@ -160,8 +160,9 @@ class Doctrine_AuditLog extends Doctrine_Record_Generator
             $conditions[] = $className . '.' . $id . ' = ?';
             $values[] = $record->get($id);
         }
-        if ($this->_options['table']->hasColumn('deleted_at')) {
-            $conditions[] = '(' . $className . '.deleted_at IS NULL OR ' . $className . '.deleted_at IS NOT NULL)';
+        if ($this->_options['table']->hasTemplate('SoftDelete')) {
+            $columnName = $this->_options['table']->getTemplate('SoftDelete')->getOption('name');
+            $conditions[] = '(' . $className . '.' . $columnName . ' IS NULL OR ' . $className . '.' . $columnName . ' IS NOT NULL)';
         }
 
         // Lock the version table using 'FOR UPDATE'
